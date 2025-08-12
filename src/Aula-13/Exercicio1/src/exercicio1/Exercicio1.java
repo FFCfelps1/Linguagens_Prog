6fcb6063182c353682013a9cb82e2cd5d4174c55
Felipe Fazio da Costa <felipefazio.cossta@gmail.com>
2025-08-12 11:41:05 -0300

    First class neatbeans

---
+++ src/Aula-13/Exercicio1/src/exercicio1/Exercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio1;
+
+/**
+ *
+ * @author felip
+ */
+public class Exercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TelaEx1 tela = new TelaEx1();
+    }
+    
+}
