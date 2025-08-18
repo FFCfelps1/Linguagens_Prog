33a8e689a220ba14fc50bb1a5b80b1b7b50325fb
FFCfelps1 <felipefazio.cossta@gmail.com>
2025-08-18 10:16:11 -0300

    exercicio 3 e 4

---
+++ src/Aula-13/Exercicio3_e_4/src/aula13_ex3_e_4/Aula13_Ex3_e_4.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula13_ex3_e_4;
+
+
+public class Aula13_Ex3_e_4 {
+
+
+    public static void main(String[] args) {
+        Tela tela = new Tela();
+    }
+    
+}
